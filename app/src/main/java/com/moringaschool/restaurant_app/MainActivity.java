6e8fc2a58616df7d1d9a3dd107318252ff973f3c
j23747 <josphatmwania237@gmail.com>
2022-05-08 22:47:46 +0300

    Add onCreate method

--- app/src/main/java/com/moringaschool/restaurant_app/MainActivity.java
+++ app/src/main/java/com/moringaschool/restaurant_app/MainActivity.java
@@ -3,12 +3,18 @@ package com.moringaschool.restaurant_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
     }
 }
