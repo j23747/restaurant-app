dccb6c0c29476200f0350aaec86818e32ea33597
j23747 <josphatmwania237@gmail.com>
2022-05-08 22:51:47 +0300

    Add click listeners

--- app/src/main/java/com/moringaschool/restaurant_app/MainActivity.java
+++ app/src/main/java/com/moringaschool/restaurant_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.moringaschool.restaurant_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -16,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
+        //Adding click listeners
+
+             mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+
+                 }
+             });
+
+
     }
 }
