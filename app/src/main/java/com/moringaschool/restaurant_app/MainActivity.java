beb0163c18fc7ece331ec7c0526e05f7b821ca27
j23747 <josphatmwania237@gmail.com>
2022-05-14 06:42:36 +0300

    ft-added EditText view

--- app/src/main/java/com/moringaschool/restaurant_app/MainActivity.java
+++ app/src/main/java/com/moringaschool/restaurant_app/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.restaurant_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 Toast.makeText(MainActivity.this, "Find restaurants!", Toast.LENGTH_LONG).show();
+                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                 startActivity(intent);
+//                 Toast.makeText(MainActivity.this, "Find restaurants!", Toast.LENGTH_LONG).show();
 
              }
          });
